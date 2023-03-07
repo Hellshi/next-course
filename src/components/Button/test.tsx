2bcf24f94229b1ffcc11ef1e1a92bbb8f63da007
hellshi <hcslimaa@gmail.com>
2023-03-07 15:24:38 -0300

    test: button tests

--- src/components/Button/test.tsx
+++ src/components/Button/test.tsx
@@ -1,13 +1,36 @@
-import { render, screen } from '@testing-library/react'
+import { RenderWithTheme } from '../../utils/tests/helpers'
+import { screen } from '@testing-library/react'
 
 import Button from '.'
 
 describe('<Button />', () => {
-  it('should render the heading', () => {
-    const { container } = render(<Button />)
+  it('should render the mid size button by default', () => {
+    RenderWithTheme(<Button> Buy Now </Button>)
 
-    expect(screen.getByRole('heading', { name: /Button/i })).toBeInTheDocument()
+    expect(screen.getByRole('button', { name: /Buy Now/i })).toHaveStyle({
+      height: '4rem',
+      padding: '0.8rem 3.2rem',
+      'font-size': '1.4rem'
+    })
+  })
 
-    expect(container.firstChild).toMatchSnapshot()
+  it('should render the small size button', () => {
+    RenderWithTheme(<Button size="small"> Buy Now </Button>)
+
+    expect(screen.getByRole('button', { name: /Buy Now/i })).toHaveStyle({
+      height: '3rem',
+      padding: '0.8rem',
+      'font-size': '1.2rem'
+    })
+  })
+
+  it('should render the large size button', () => {
+    RenderWithTheme(<Button size="large"> Buy Now </Button>)
+
+    expect(screen.getByRole('button', { name: /Buy Now/i })).toHaveStyle({
+      height: '5rem',
+      padding: '0.8rem 4.0rem',
+      'font-size': '1.6rem'
+    })
   })
 })
