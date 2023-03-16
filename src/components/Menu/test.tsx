ae37e0e4bd98360a38883afdde3f4f38eb2e6ed5
hellshi <hcslimaa@gmail.com>
2023-03-16 15:51:36 -0300

    test: menu tests

--- src/components/Menu/test.tsx
+++ src/components/Menu/test.tsx
@@ -1,13 +1,14 @@
+import { RenderWithTheme } from '../../utils/tests/helpers'
 import { render, screen } from '@testing-library/react'
 
 import Menu from '.'
 
 describe('<Menu />', () => {
-  it('should render the heading', () => {
-    const { container } = render(<Menu />)
+  it('should render the menu', () => {
+    RenderWithTheme(<Menu />)
 
-    expect(screen.getByRole('heading', { name: /Menu/i })).toBeInTheDocument()
-
-    expect(container.firstChild).toMatchSnapshot()
+    expect(screen.getByLabelText(/open menu/i)).toBeInTheDocument()
+    expect(screen.getByLabelText(/search/i)).toBeInTheDocument()
+    expect(screen.getByLabelText(/shopping cart/i)).toBeInTheDocument()
   })
 })
