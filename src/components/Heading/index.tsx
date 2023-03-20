4c90e1124900a6e594c339fcfa939febbe7d71e4
hellshi <hcslimaa@gmail.com>
2023-03-20 11:33:18 -0300

    feat: ensure lines and size are flexible

--- src/components/Heading/index.tsx
+++ src/components/Heading/index.tsx
@@ -5,15 +5,27 @@ export type HeadingProps = {
   color?: 'black' | 'white'
   lineLeft?: boolean
   lineBottom?: boolean
+  size?: 'small' | 'medium'
+  lineColor?: LineColors
 }
+
+export type LineColors = 'primary' | 'secondary'
 
 const Heading = ({
   children,
   color = 'white',
   lineLeft = false,
-  lineBottom = false
+  lineBottom = false,
+  size = 'medium',
+  lineColor = 'primary'
 }: HeadingProps) => (
-  <S.Wrapper color={color} lineLeft={lineLeft} lineBottom={lineBottom}>
+  <S.Wrapper
+    color={color}
+    lineLeft={lineLeft}
+    lineBottom={lineBottom}
+    size={size}
+    lineColor={lineColor}
+  >
     {children}
   </S.Wrapper>
 )
