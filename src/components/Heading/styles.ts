02bbc05678b0e51bd615ab95629211b321524c00
hellshi <hcslimaa@gmail.com>
2023-03-20 11:33:18 -0300

    fea: ensure lines and size are flexible

--- src/components/Heading/styles.ts
+++ src/components/Heading/styles.ts
@@ -1,13 +1,13 @@
 import styled, { css, DefaultTheme } from 'styled-components'
 import media from 'styled-media-query'
-import { HeadingProps } from '.'
+import { HeadingProps, LineColors } from '.'
 
 const WrapperModifiers = {
-  lineLeft: (theme: DefaultTheme) => css`
+  lineLeft: (theme: DefaultTheme, lineColor: LineColors) => css`
     padding-left: ${theme.spacings.xxsmall};
-    border-left: 0.7rem solid ${theme.colors.secondary};
+    border-left: 0.7rem solid ${theme.colors[lineColor]};
   `,
-  lineBottom: (theme: DefaultTheme) => css`
+  lineBottom: (theme: DefaultTheme, lineColor: LineColors) => css`
     position: relative;
     margin-bottom: ${theme.spacings.medium};
     &::after {
@@ -16,13 +16,22 @@ const WrapperModifiers = {
       left: 0;
       bottom: -1rem;
       width: 5rem;
-      border-bottom: 0.5rem solid ${theme.colors.primary};
+      border-bottom: 0.5rem solid ${theme.colors[lineColor]};
+    }
+  `,
+  medium: (theme: DefaultTheme) => css`
+    font-size: ${theme.font.sizes.xxlarge};
+  `,
+  small: (theme: DefaultTheme) => css`
+    font-size: ${theme.font.sizes.medium};
+    &::after {
+      width: 3rem;
     }
   `
 }
 
 export const Wrapper = styled.h2<HeadingProps>`
-  ${({ theme, color, lineLeft, lineBottom }) => css`
+  ${({ theme, color, lineLeft, lineBottom, size, lineColor }) => css`
     color: ${theme.colors[color!]};
 
     font-size: ${theme.font.sizes.xlarge};
@@ -31,8 +40,10 @@ export const Wrapper = styled.h2<HeadingProps>`
         font-size: ${theme.font.sizes.xxlarge};
     `};
 
-    ${lineLeft && WrapperModifiers.lineLeft(theme)};
+    ${lineLeft && WrapperModifiers.lineLeft(theme, lineColor!)};
 
-    ${lineBottom && WrapperModifiers.lineBottom(theme)};
+    ${lineBottom && WrapperModifiers.lineBottom(theme, lineColor!)};
+
+    ${!!size && WrapperModifiers[size](theme)}
   `}
 `
